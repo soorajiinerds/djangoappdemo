de92a4241e16070faacda1ed2a808688824b2aa8
sooraj ps <sooraj.p@iinerds.com>
2024-04-15 12:16:50 +0530

    th commit

--- notes/document/views.py
+++ notes/document/views.py
@@ -21,7 +21,7 @@ def editor(request):
  
         if docid > 0:
             note = Note.objects.get(pk=docid)
-            note.title = title
+            note.title = "title"
             note.content = content
             note.save()
  
@@ -50,7 +50,6 @@ def editor(request):
 @login_required(login_url='/login/')
 def delete_note(request, docid):
     note = Note.objects.get(pk=docid)
-    # note.delete()
  
     return redirect('/?docid=0')
  
